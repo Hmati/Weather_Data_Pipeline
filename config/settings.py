37d75892f9c1635d7ba0ed7e5b4b58d369858f34
Hmati <mosesmati93@gmail.com>
2024-11-14 23:54:32 +0300

    Add all files

---
+++ config/settings.py
@@ -0,0 +1,10 @@
+# config/settings.py
+
+# OpenWeatherMap API Key
+API_KEY = "58baf0add2f0a3c410f41ba99ef81e62"
+
+# City for which you want to collect weather data
+CITY = "Nairobi"
+
+# Database URI for PostgreSQL connection
+DATABASE_URI = "postgresql+psycopg2://user:hellen@localhost:5432/weather"
